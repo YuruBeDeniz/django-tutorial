c3636cf553163af2e01ac5b156dbe9a088d8b540
Deniz Saganak <saganak.deniz@gmail.com>
2024-09-07 11:51:09 +0200

    user login and auth

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register_view, name="registeration"),
+    path('register/', views.register_view, name="registration"),
+    path('login/', views.login_view, name="login"),
 ]
